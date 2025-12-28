5b1302c468dddbcf1464af2703d7e35a5352aa24
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2025-12-28 15:36:13 -0300

    [MIG] document_page

--- document_page/__manifest__.py
+++ document_page/__manifest__.py
@@ -19,14 +19,7 @@
      "version": "19.0.1.0.0",
      "category": "Knowledge Management",
      "author": "OpenERP SA, Odoo Community Association (OCA)",
-     "images": [
-          "images/category_list.png",
-          "images/create_category.png",
-          "images/page_list.png",
-          "images/create_page.png",
-          "images/customer_invoice.jpeg",
-          "images/page_history.png",
-     ],
+     "images": [],
      "website": "https://github.com/OCA/knowledge",
      "license": "AGPL-3",
      "depends": ["mail", "document_knowledge", "web_editor"],
@@ -40,7 +33,7 @@
           "views/document_page_history.xml",
           "views/report_document_page.xml",
      ],
-     "demo": ["demo/document_page.xml"],
+     "demo": [],
      "assets": {
           "web._assets_primary_variables": [
                "document_page/static/src/**/document_page_variables.scss",
