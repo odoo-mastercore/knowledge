588f19fa8cdf9ac18d18183cf813819588e4bfe6
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2025-12-28 23:32:51 -0300

    [MIG] document_page

--- document_page/models/document_page_history.py
+++ document_page/models/document_page_history.py
@@ -9,7 +9,8 @@
 # Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
+
 from odoo.addons.html_editor.models.diff_utils import (
     generate_comparison,
 )
